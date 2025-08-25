4a03f95f664e780bee3bbc5bee9e919cdec21d33
Hansol Yoon <yoondev3434@gmail.com>
2025-08-25 12:20:44 +0900

    feat: 클릭 이벤트 리스너 추가 및 페이지 전환 기능 구현

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -9,8 +9,20 @@ export default function IntroPage() {
     >("intro");
 
     React.useEffect(() => {
-        //
-    }, []);
+        const handleClick = () => {
+            if (currentPage === "intro") {
+                setCurrentPage("invitation");
+            }
+        };
+
+        // 클릭 이벤트 리스너 추가
+        document.addEventListener("click", handleClick);
+
+        // 클린업 함수: 컴포넌트 언마운트 시 이벤트 리스너 제거
+        return () => {
+            document.removeEventListener("click", handleClick);
+        };
+    }, [currentPage]);
 
     return (
         <div
